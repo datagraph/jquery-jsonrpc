f917f6ea897b6449417a1515abb21f63829206a5
Josh Huckabee <joshhuckabee@gmail.com>
2010-10-25 00:54:42 +0200

    Add README, UNLICENSE, and pluging source.

---
+++ README.md
@@ -0,0 +1,75 @@
+# jQuery JSON RPC 2.0
+
+A JSON RPC 2.0 compatible jQuery plugin.
+
+## Getting Started
+
+The simplest way to configure jsonRPC is via the setup method
+
+    $.jsonRPC.setup({
+      endPoint: '/rpc',
+      namespace: 'datagraph'
+    });
+
+Once you've configured an end point, making requests is a matter of
+
+    $.jsonRPC.request('method.name', params, {
+      success: function(result) {
+        // Do something with the result here
+        // It comes back as an RPC 2.0 compatible response object
+      },
+      error: function(result) {
+        // Result is an RPC 2.0 compatible response object
+      }
+    });
+
+Want to do batch requests?  We've got you covered
+
+    $.jsonRPC.batchRequest([
+        {
+          method: 'method.one',
+          params: [1,2,3]
+        },
+        {
+          method: 'method.two'
+        }
+      ], { 
+        success: function(result) {
+          // Handle response object here
+        }
+      }
+    });
+
+Need to temporarily override your end point or namespace?  Easy enough...
+
+    $.jsonRPC.withOptions({
+      endPoint: '/anotherRpc',
+      namespace: 'somethingElse'
+    }, function() {
+      this.request('method.name');
+    });
+
+And after your call, your entPoint and namespace are back to their defaults.  Or you can override the end point for a single request via
+
+    $.jsonRPC.request('method.name', [1,2,3], null, '/anotherEndPoint');
+
+## Authors, Development, License, Thanks
+
+#### Authors
+ * Josh Huckabee <joshhuckabee@gmail.com>
+
+#### 'License'
+This plugin is free and unemcumbered software released into the public
+domain.  For more information, see the included UNLICENSE file.
+
+#### Contributing
+Fork it on Github and go.  Please make sure you're kosher with the UNLICENSE
+file before contributing.
+
+#### Thanks
+Inspiration for this plugin came from the following existing plugins:
+
+* http://plugins.jquery.com/project/C4PCJsonRPC
+* http://plugins.jquery.com/project/jsonRPC2
+
+Thanks to [safchain]:http://plugins.jquery.com/users/safchain and [kusmierz]:http://plugins.jquery.com/user/30124 for their efforts.
---
+++ jquery.jsonrpc.js
@@ -0,0 +1,143 @@
+(function($) {
+  $.extend({
+    jsonRPC: {
+      // RPC Version Number
+      version: '2.0',
+
+      // End point path, sets a default for all requests
+      // This can be initialized in the setup manually, via
+      // the setup method or overriden on a per request basis
+      endPoint: null,
+
+      // Default namespace for methods, this can be set manually
+      // or via the setup method
+      namespace: null,
+
+      // Setup the endPoint and namespace params with defaults
+      setup: function(params) {
+        if(typeof(params) !== 'undefined') {
+          if (typeof(params.endPoint) !== 'endPoint') this.endPoint = params.endPoint;
+          if (typeof(params.namespace) !== 'undefined') this.namespace = params.namespace;
+        }
+      },
+
+      // Convenience wrapper method to allow you to temporarily set a config parameter
+      // (endPoint or namespace) for RPC calls performed within the passed in callback
+      withOptions: function(params, callback) {
+        origParams = {endPoint: this.endPoint, namespace: this.namespace};
+        this.setup(params);
+        callback.call(this);
+        this.setup(origParams);
+      },
+
+      // Performas a single RPC request
+      request: function(method, params, callbacks, url) {
+        if (typeof(method) !== 'undefined') {
+          this._doRequest(JSON.stringify(this._requestDataObj(method, params, 1)),
+                          callbacks,
+                          url);
+        }
+        else {
+          throw "Invalid method supplied for jsonRPC request";
+        }
+      },
+
+      // Submits multiple requests
+      // Takes an array of objects that contain a method and params
+      batchRequest: function(requests, callbacks, url) {
+        if(typeof(requests) === 'undefined' || !requests || !requests.length) return this._response();
+
+        var data = [],
+            request;
+
+        for(var i = 0; i<requests.length; i++) {
+          request = requests[i];
+          data.push(this._requestDataObj(request.method, request.params, i+1));
+        }
+        console.log(JSON.stringify(data));
+
+        this._doRequest(JSON.stringify(data), callbacks, url);
+      },
+
+      // Internal method used for generic ajax requests
+      _doRequest: function(data, callbacks, url) {
+        var _that = this;
+        $.ajax({
+          type: 'POST',
+          url: this._requestUrl(url),
+          data: data,
+          cache: false,
+          processData: false,
+          contentType: 'application/json',
+          error: function(json) {
+            _that._requestError.call(_that, callbacks, json);
+          },
+          success: function(json) {
+            _that._requestSuccess.call(_that, callbacks, json);
+          }
+        })
+      },
+
+      // Determines the appropriate request URL to call for a request
+      _requestUrl: function(url) {
+        url = url || this.endPoint;
+        return url + '?tm=' + new Date().getTime()
+      },
+
+      // Creates an RPC suitable request object
+      _requestDataObj: function(method, params, id) {
+        return {
+          jsonrpc: this.version,
+          method: this.namespace ? this.namespace +'.'+ method : method,
+          params: params,
+          id: id
+        }
+      },
+
+      // Handles calling of error callback function
+      _requestError: function(callbacks, json) {
+        if (typeof(callbacks) !== 'undefined' && typeof(callbacks.error) !== 'undefined') {
+            callbacks.error(this._response());
+        }
+      },
+
+      // Handles calling of RPC success, calls error callback
+      // if the response contains an error
+      _requestSuccess: function(callbacks, json) {
+        var response = this._response(json);
+        response.error && callbacks.error ? callbacks.error(response) : null;
+        callbacks.success ? callbacks.success(response) : null;
+      },
+
+      // Returns a generic RPC 2.0 compatible response object
+      _response: function(json) {
+        if (typeof(json) === 'undefined') {
+          return {
+            error: 'Internal server error',
+            version: '2.0'
+          };
+        }
+        else {
+          try {
+            if(typeof(json) === 'string') {
+              json = eval ( '(' + json + ')' );
+            }
+
+            if (json.jsonrpc != '2.0') {
+              throw 'Version error';
+            }
+
+            return json;
+          }
+          catch (e) {
+            return {
+              error: 'Internal server error: ' + e,
+              version: '2.0'
+            }
+          }
+        }
+      }
+
+    }
+  });
+})(jQuery);
